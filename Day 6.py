0c238a950b9c84b9d25e45042505694cb3f242c7
Mariana Baroni <68928929+mbaronif@users.noreply.github.com>
2024-09-07 11:38:45 +0900

    Create Day 6.py

---
+++ Day 6.py
@@ -0,0 +1,9 @@
+userName = input("What is your name? ")
+if userName == "Jack" or userName == "jack":
+  print("Hello Jack!")
+elif userName == "Pat" or userName == "pat":
+  print("Hey there Pat!")
+elif userName == "Sue" or userName == "sue":
+  print("Nice to see you Sue!")
+else:
+  print("Sorry, never seen you in my life.")
