6dc6627b0f89ce408a5678f1bfa180d9c600824e
Mariana Baroni <68928929+mbaronif@users.noreply.github.com>
2024-10-23 21:52:01 +0900

    Create Day 36 - People List.py

---
+++ Day 36 - People List.py
@@ -0,0 +1,20 @@
+uniqueList = [] # My library
+
+def nameList(): # Prints the full list, with new additions.
+    for name in uniqueList:
+        print(name)
+    print()
+
+while True: # Prompts the input requests
+    firstName = input("Name: ").strip().title() # Deletes extra spaces and capitalizes the first letter. In this case, could be .capitalize too.
+    lastName = input("Surname: ").strip().title()
+    print()
+
+    fullName = (f"{firstName} {lastName}") # Concatenates both inputs in a string, separated with a single space.
+
+    if fullName not in uniqueList: # Checks the input name and surname to prevent duplicates. If the name is repeted, it'll not be added.
+        uniqueList.append(fullName)
+    nameList()
+
+    if not firstName or not lastName: # Just a safe measure to avoid infinite loop.
+        break
