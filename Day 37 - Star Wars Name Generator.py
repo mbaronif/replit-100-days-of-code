2f7e1a61acaba863be5cd2337fd309a7a06ab2a1
Mariana Baroni <68928929+mbaronif@users.noreply.github.com>
2024-10-25 17:58:02 +0900

    Update Day 37 - Star Wars Name Generator.py
    Added alternative ways to solve the problem, just for the record.

--- Day 37 - Star Wars Name Generator.py
+++ Day 37 - Star Wars Name Generator.py
@@ -15,3 +15,28 @@ citySW = cityBorn[:4:-1] # The only one which is "counting" characters correctly
 starWarsName = f"{firstSW}{lastSW} {mumSW}{citySW}"
 
 print(starWarsName)
+
+"""
+# Alternative 1:
+
+firstName = input("First name: ").strip()
+lastName = input("Last name: ").strip()
+mumsName = input("Mum's name: ").strip()
+cityBorn = input("Place where you were born: ").strip()
+
+starWarsName = f"{firstName[:3].title()}{lastName[:3].lower()} {mumsName[:2].title()}{cityBorn[:4:-1].lower()}"
+print(starWarsName)
+
+# Alternative 2
+# Although simpler, more error-prone
+
+all = input("Enter your first name, last name, mum's maiden name, and the city you were born in: ").split()
+
+first = all[0].strip()
+last = all[1].strip()
+maiden = all[2].strip()
+city = all[3].strip()
+
+starWarsName = f"{first[:3].title()}{last[:2].lower()} {maiden[:2].title()}{city[:4:-1].lower()}"
+print(starWarsName)
+"""
