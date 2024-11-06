d9219db4a3795a3820dc15cf8d5304c5c820ab4e
Mariana Baroni <68928929+mbaronif@users.noreply.github.com>
2024-11-06 22:40:44 +0900

    Update Day 39 - Hangman.py

--- Day 39 - Hangman.py
+++ Day 39 - Hangman.py
@@ -47,9 +47,65 @@ while True:
     print("Letters tried so far: ", letterBank)
     print(f"Current word: {guess}")
 
+
+# Working version (solution)
+import random
+
+hangmanWords = [
+    "apple", "banana", "strawberry", "orange", "grapes",
+    "avocado", "mango", "melon", "cherry", "pear"]
+
+#Creates a list of letters that have already been used in the game, right or wrong.
+letterBank = []
+
+lives = 6
+
+word = random.choice(hangmanWords) # Picks a word randomly
+
+while True:
+    character = input("Choose a letter: ").lower() # Makes sure all letter inputs are accepted
+
+# Checks if the letter was already picked
+    if character in letterBank:
+        print("You've tried that before")
+# If True, the player is prompted for a new letter
+        continue
+
+# Adds the letters picked to the letterBank, if it was not a repetition
+    letterBank.append(character)
+
+# Checks if the character is in the chosen word
+    if character in word:
+        print("You found a letter")
+    else:
+        print("Nope, not in there")
+        lives -= 1
     
-    #    letterBank.append(character)
-    #    if character in letterBank:
-    #    print("That letter was already tried.")
-    #    continue
-    
+    allLetters = True
+
+# This part is unindented because it has to occur regardless of the 2 if statements above
+    print()
+# If it's in the word, it'll check every position of the word
+    for letter in word:
+        if letter in letterBank:
+# The letter is printed in the right position
+            print(letter, end="")
+        else:
+# The gap sign will be put in place of the undiscovered letters
+            print("_ ", end="")
+            allLetters = False
+    print()
+
+# Checks if the word is complete
+    if allLetters:
+        print(f"You won with {lives} left!")
+        break
+
+# Checks the number of lives still available
+    if lives <= 0:
+        print(f"You ran out of lives. The answer was {word}.")
+# If the player's out of lives, the game is broken
+        break
+# Tells the player how many lives they have left, if they haven't ran out of lives yet
+    else:
+        print(f"Only {lives} left.")
