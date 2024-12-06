eede89a7acd1f224d37942f6170afed02dddc3a0
Mariana Baroni <68928929+mbaronif@users.noreply.github.com>
2024-12-06 19:36:39 +0900

    Create Day 45 - ToDo List System.py
    Tasks:
    * Menu: add, view, edit, remove a task
    - [ ] Add: 
    	* what the task is
    	* deadline
    	* priority (high, medium, low)
    - [ ] View:
    	* view all - all the tasks (can be done with a Pretty Print def)
    	* view a priority - only shows the tasks with that priority
    - [ ] Edit:
    	* change any info in a task
    - [ ] Remove:
    	* remove a task

---
+++ Day 45 - ToDo List System.py
@@ -0,0 +1,101 @@
+import time, os
+
+toDoList = ["Task" : None, "Deadline" : None, "Priority" : None]
+
+def prettyPrint():
+    print("ToDo List Management System")
+    print()
+    print("1: Add\n2: View\n3: Edit\n4: Remove")
+    print()
+
+def printList():
+    for task in toDoList:
+        print(task)
+    print()
+
+while True:
+    prettyPrint()
+    userAction = input("> ")
+    
+    if userAction == "1":
+        print("Add")
+        print()
+        task = input("What do you want to add?\n")
+        if task in toDoList: # Checks if the task already exists in the list.
+            print()
+            print("Sorry, the task is already in the list.")
+            retryOption = input("Do you want to add another task?\n") # In case the user still wants to add a new task to the list.
+            if retryOption.strip().lower() == "yes":
+                task = input("What do you want to add?\n") # Prompts for the task to be added to the list.
+                toDoList.append(task.capitalize())
+            else:
+                continue # Restarts the program.
+        else:
+            toDoList.append(task.capitalize())
+        print()
+        
+    elif userAction == "2":
+        print("View")
+        print()
+        printList()
+        time.sleep(2)
+    
+    elif userAction == "3":
+        print("Edit")
+        print()
+        task = input("What do you want to edit?\n")
+        if task in toDoList:
+            removed = toDoList.index(task) # Checks the position of the task to be removed.
+            toDoList.remove(task) # Removes the task from the list.
+            task = input("What will be the new task?\n")
+            if task in toDoList: # Checks if the task already exists in the list.
+                print()
+                print("Sorry, the task is already in the list.")
+                retryOption = input("Do you want to add another task?\n") # In case the user still wants to add a new task to the list.
+                if retryOption.strip().lower() == "yes":
+                    task = input("What do you want to add?\n")
+                    toDoList.insert(removed, task.capitalize()) # Adds the new task to the place of the removed task.
+                else:
+                    continue
+            else:
+                toDoList.insert(removed, task.capitalize()) # Adds the new task to the place of the removed task.
+            print()
+        else:
+            print(f"Sorry, {task} is not in the list.")
+            retryRemoveOption = input("Do you want to edit another task?\n")
+            if retryRemoveOption.strip().lower() == "yes":
+                task = input("What do you want to edit?\n")
+                removed = toDoList.index(task) # Checks the position of the task to be removed.
+                toDoList.remove(task) # Removes the task from the list.
+                task = input("What will be the new task?\n")
+                if task in toDoList: # Checks if the task already exists in the list.
+                    print()
+                    print("Sorry, the task is already in the list.")
+                    retryOption = input("Do you want to add another task?\n") # In case the user still wants to add a new task to the list.
+                    if retryOption.strip().lower() == "yes":
+                        task = input("What do you want to add?\n")
+                        toDoList.insert(removed, task.capitalize()) # Adds the new task to the place of the removed task.
+                    else:
+                        continue
+            else:
+                continue # Restarts the program.
+    elif userAction == "4":
+        print("Remove")
+        print()
+        task = input("What do you want to remove?\n")
+        if task in toDoList: # Checks if the task exists in the list.
+            doubleCheck = input(f"Are you sure you want to remove {task}?\n") # Confirmation of removal
+            if doubleCheck.strip().lower() == "yes":
+                toDoList.remove(task)
+                print()
+                print(f"{task} was removed from the list.")
+            else:
+                continue # Restarts the program.
+        else:
+            print(f"{task} is not in the list.\n")
+            print()
+    else:
+        print("Thanks for using the ToDo List Management System")
+        break
+    time.sleep(0.5)
+    os.system("clear")
