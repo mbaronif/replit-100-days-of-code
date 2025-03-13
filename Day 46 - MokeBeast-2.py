530d044d1fb4b319ce026e7ad114d502314b0e26
Mariana Baroni <maribaroni@gmail.com>
2025-03-13 19:26:32 +0100

    Finished Day 46 exercise

---
+++ Day 46 - MokeBeast-2.py
@@ -0,0 +1,20 @@
+mokeBeast = {}
+
+#Feeds the dictionary
+def prettyPrint():
+    print(f"Name\tType\tHP\tMP")
+    for key, value in mokeBeast.items():
+        print(f"""{key:^6}|{value["Type"]:^6}|{value["HP"]:^6}|{value["MP"]:^8}""")
+    print()
+
+while True:
+    print("Enter the MokeBeast's information:")
+    #Feeds the dictionary
+    name = input("Name: ").title()
+    type = input("Type: ").title()
+    hp = int(input("HP: "))
+    mp = int(input("MP: "))
+    mokeBeast[name] = {"Type": type, "HP": hp, "MP": mp}
+    print()
+    #Prints all entries in the Type respective color
+    prettyPrint()
