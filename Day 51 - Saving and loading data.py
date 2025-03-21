7c031147e2b4f2a4dc4b3a9b89b85c86e967c0d6
Mariana Baroni <maribaroni@gmail.com>
2025-03-21 16:29:11 +0100

    Done some improvements to UX

--- Day 51 - Saving and loading data.py
+++ Day 51 - Saving and loading data.py
@@ -1,3 +1,5 @@
+import time, os
+
 toDoList = []
 
 f = open("ToDoList.txt", "r")
@@ -13,78 +15,97 @@ while True:
     print("ToDo List Manager")
     print()
     # Added option "remove" and replaced "edit" with "change", which I think makes it clearer.
-    menu = input("Do you want to view, add, remove, change, or clear your ToDo list?\n")
+    menu = input("This is your To Do list. What do you want to do?\n1: view\n2: add\n3: change\n4: remove\n5: clear\n6: exit\n\n> ")
     print()
-    if menu == "view":
+    if menu == "1":
         printList()
-    elif menu == "add":
-        item = input("What do you want to add?\n")
+        time.sleep(2)
+        os.system("clear")
+    elif menu == "2":
+        item = input("What do you want to add?\n> ")
         if item in toDoList: # Checks if the item already exists in the list.
             print()
             print("Sorry, the item is already in the list.")
-            retryOption = input("Do you want to add another item?\n") # In case the user still wants to add a new item to the list.
+            retryOption = input("Do you want to add another item?\n> ") # In case the user still wants to add a new item to the list.
             if retryOption == "yes":
-                item = input("What do you want to add?\n") # Prompts for the item to be added to the list.
+                item = input("What do you want to add?\n> ") # Prompts for the item to be added to the list.
                 toDoList.append(item)
             else:
                 continue # Restarts the program.
         else:
             toDoList.append(item)
+        time.sleep(1)
+        os.system("clear")
         print()
-    elif menu == "change":
-        item = input("What do you want to change?\n")
+    elif menu == "3":
+        item = input("What do you want to change?\n> ")
         if item in toDoList:
             removed = toDoList.index(item) # Checks the position of the item to be removed.
             toDoList.remove(item) # Removes the item from the list.
-            item = input("What will be the new item?\n")
+            item = input("What will be the new item?\n> ")
             if item in toDoList: # Checks if the item already exists in the list.
                 print()
                 print("Sorry, the item is already in the list.")
-                retryOption = input("Do you want to add another item?\n") # In case the user still wants to add a new item to the list.
+                retryOption = input("Do you want to add another item?\n> ") # In case the user still wants to add a new item to the list.
                 if retryOption == "yes":
-                    item = input("What do you want to add?\n")
+                    item = input("What do you want to add?\n> ")
                     toDoList.insert(removed, item) # Adds the new item to the place of the removed item.
                 else:
                     continue
             else:
                 toDoList.insert(removed, item) # Adds the new item to the place of the removed item.
-            print()
+            print()           
         else:
             print(f"Sorry, {item} is not in the list.")
-            retryRemoveOption = input("Do you want to remove another item?\n")
+            retryRemoveOption = input("Do you want to change another item?\n> ")
             if retryRemoveOption == "yes":
-                item = input("What do you want to remove?\n")
+                item = input("What do you want to change?\n> ")
                 removed = toDoList.index(item) # Checks the position of the item to be removed.
                 toDoList.remove(item) # Removes the item from the list.
-                item = input("What will be the new item?\n")
+                item = input("What will be the new item?\n> ")
                 if item in toDoList: # Checks if the item already exists in the list.
                     print()
                     print("Sorry, the item is already in the list.")
-                    retryOption = input("Do you want to add another item?\n") # In case the user still wants to add a new item to the list.
+                    retryOption = input("Do you want to add another item?\n> ") # In case the user still wants to add a new item to the list.
                     if retryOption == "yes":
-                        item = input("What do you want to add?\n")
+                        item = input("What do you want to add?\n> ")
                         toDoList.insert(removed, item) # Adds the new item to the place of the removed item.
                     else:
                         continue
             else:
                 continue # Restarts the program.
-    elif menu == "remove":
-        item = input("What do you want to remove?\n")
+            time.sleep(1)
+            os.system("clear") 
+    elif menu == "4":
+        item = input("What do you want to remove?\n> ")
         if item in toDoList: # Checks if the item exists in the list.
-            doubleCheck = input(f"Are you sure you want to remove {item}?\n") # Confirmation of removal
+            doubleCheck = input(f"Are you sure you want to remove {item}?\n> ") # Confirmation of removal
             if doubleCheck == "yes":
                 toDoList.remove(item)
+                print(f"{item} has been removed from the list.")
                 print()
             else:
                 continue # Restarts the program.
         else:
             print(f"{item} is not in the ToDo menu:\n")
             print()
-    elif menu == "clear": # Clears the items on the list.
+        time.sleep(1)
+        os.system("clear") 
+    elif menu == "5": # Clears the items on the list.
         toDoList.clear()
+        print("The list has been cleared.")
         print()
+        time.sleep(1)
+        os.system("clear") 
+    elif menu == "6":
+        print("Thanks for using the Todo menu Manager")
+        time.sleep(2)
+        os.system("clear") 
+        break
     else:
         print("Thanks for using the Todo menu Manager")
+        time.sleep(2)
+        os.system("clear")
         break
 
 f = open("ToDoList.txt", "w")
