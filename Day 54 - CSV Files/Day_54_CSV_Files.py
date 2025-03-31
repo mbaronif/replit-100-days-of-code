220a0e0fc88c8fd31f767379b612c35e75a233b0
Mariana Baroni <maribaroni@gmail.com>
2025-03-31 15:18:03 +0200

    Updated output (0h51)

--- Day 54 - CSV Files/Day_54_CSV_Files.py
+++ Day 54 - CSV Files/Day_54_CSV_Files.py
@@ -7,18 +7,38 @@ from the proposed exercise as well.
 import csv
 # Writing to a CSV file
 
+income = 0
+expenditure = 0
+netTotal = 0
+
 #We have to add the full path for the file to be accessed.
 with open("/Users/marianabaronifontana/Projects/replit-100-days-of-code/Day 54 - CSV Files/January.csv") as file:   
     reader = csv.DictReader(file)
+    # Prompts the user for a date in the format DD/MM/YYYY.
+    #selected_date = input("Enter the date (DD/MM/YYYY): ")
+    
     for row in reader:
-        #Some exercises to test the code:
-        #print(row)
-        #print(row["Date"], row["Net Total"])
-        #print(row["Date"], row["Net Total"], row["Income"], row["Expenditure"])
+        income += float(row["Income"])
+        expenditure += float(row["Expenditure"])
+        netTotal += float(row["Net Total"])
 
+    #Total Income
+    print(f"\nTotal Income: ${income}")
+    #Total Expenditure
+    print(f"Total Expenditure: ${expenditure}")
+    #Net total
+    print(f"Net Total: ${netTotal}")
+    #January's average income
+    print(f"Average Income: ${income/31}")
+    #January's average expenditure
+    print(f"Average Expenditure: ${expenditure/31}")
+    #January's average net total
+    print(f"Average Net Total: ${netTotal/31}")
+    print()
 
-        #To do:
-        #Find the total income and expenditure for the month of January.
-        #Find the average income and expenditure for the month of January.
-        #Find the total income and expenditure for each day of the month.
-        #Find the average income and expenditure for each day of the month.
+    file.seek(0)  # Reset file pointer to the beginning. It's necessary to reinitialize the reader
+    # Find the total income for the selected date.
+    reader = csv.DictReader(file)  # Reinitialize the reader
+    for row in reader:
+        print(f"Date: {row['Date']} Income: ${row['Income']}")
+            
