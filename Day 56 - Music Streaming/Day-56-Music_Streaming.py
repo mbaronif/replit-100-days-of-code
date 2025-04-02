94c322f9cd850cce95bb42713e60552cc8390b34
Mariana Baroni <maribaroni@gmail.com>
2025-04-02 23:19:42 +0200

    Reduced the csv file and solved the list printing issue.

--- Day 56 - Music Streaming/Day-56-Music_Streaming.py
+++ Day 56 - Music Streaming/Day-56-Music_Streaming.py
@@ -1,15 +1,20 @@
 import csv, os
 
+# Replaces the file path with a shorter variable name.
+file_path = "/Users/marianabaronifontana/Projects/replit-100-days-of-code/Day 56 - Music Streaming/TopSongs.csv"
+
 topSongs = []
 
 # Reads the CSV file.
-with open("/Users/marianabaronifontana/Projects/replit-100-days-of-code/Day 56 - Music Streaming/TopSongs.csv", "r") as file:
+with open(file_path, "r") as file:
     # Reads the CSV file using the csv module.
-    f = csv.DictReader(file)
-    for row in f:
+    reader = csv.DictReader(file)
+    for row in reader:
         # Adds the song to the list.
-        topSongs.append(row)
-
+        #topSongs.append(f"{row['Position']} - {row['Artist']} | {row['Title']} | {row['Year']} | {row['Total']}")
+        # It should create a new location for each artist, but it didn't.
+        artist_path = os.path.join("/Users/marianabaronifontana/Projects/replit-100-days-of-code/Day 56 - Music Streaming/", row['Artist'])
+        print(artist_path)
 
 # Displays the full ranking of the songs.
 def fullRanking():
@@ -17,4 +22,7 @@ def fullRanking():
         print(song)
     print()
 
-fullRanking()
+#This is only for testing purposes.
+#fullRanking()
+
+# Creates a directory for each artist.
