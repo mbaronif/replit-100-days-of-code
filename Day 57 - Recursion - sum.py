0e7c66a6ce0e259173f8f2d8b34a5a4bde0884a8
Mariana Baroni <maribaroni@gmail.com>
2025-04-16 19:57:55 +0200

    Calculates the sum of numbers starting in n

---
+++ Day 57 - Recursion - sum.py
@@ -0,0 +1,16 @@
+value = 50
+
+def recursive_sum(n):
+    if n == 1:
+        print("Done!")
+        # Stops the recursion whan it reaches 1
+        return 1
+    
+    else:
+        # Adds the current number to the result of the recursive call
+        return n + recursive_sum(n - 1)
+    
+# Displays the total sum once recursion finishes
+result = recursive_sum(value)
+print(f"The sum of all numbers, starting with {value}, is {result}")
+
