750212e01668b3f0ccaad3fe0f2934e167697649
Mariana Baroni <maribaroni@gmail.com>
2025-05-05 22:21:36 +0200

    Added some commands to guide the development.

--- Day 62 - Private Diary.py
+++ Day 62 - Private Diary.py
@@ -5,6 +5,7 @@ pass_word = "12345"
 while True:
     print("MY PRIVATE DIARY")
     password = input("Password: ")
+    # Condition to display the menu and diary
     if password == pass_word:
         print("Welcome to your private diary!")
         print("1. Add a new entry")
@@ -14,10 +15,16 @@ while True:
     else:
         break
     
+    # Conditions to work with the diary
     if choice == "1":
+        entry = input("What are the news for today? >  ")
+        # It needs to save the entry to a db
         pass
     
     elif choice == "2":
+        # It needs to display the entries from the db
+        # Then, prompt the user wether to see the next entry or to go back to the menu
+        # Next entry must be displayed in reversed order (using reverse module)
         pass
 
     elif choice == "3":
